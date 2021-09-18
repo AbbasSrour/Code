b4daff980f7a94debee45b8a237ff1ee6fda266c
Mark <abbas.gentleman@gmail.com>
2021-09-18 23:56:12 +0300

    made a script to excute for Java and C

---
+++ C/make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+extC=".c"
+cFile=$1$extC
+execFile=$1
+
+gcc ~/College-Code/C/src/$cFile -o $execFile
+mv ~/College-Code/C/$execFile ~/College-Code/C/output
+cd ~/College-Code/C/output
+./$1
+cd ..
